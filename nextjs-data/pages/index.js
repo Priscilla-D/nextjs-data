3608cac31c7ed7f9747b6a9d3800de146e839c6d
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-25 17:15:38 +0200

    section 5-94

--- nextjs-data/pages/index.js
+++ nextjs-data/pages/index.js
@@ -12,15 +12,29 @@ function HomePage(props) {
   );
 }
 export async function getStaticProps() {
-  console.log('(Re-)Generating...');
+  console.log("(Re-)Generating...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json"); // création du chemin vers le fichier contenant les datas
   const jsonData = await fs.readFile(filePath); // lecture du fichier
   const data = JSON.parse(jsonData); // convertit le json en objet JS
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+  
+  if (data.producs.length === 0) {
+    return { notFound: true };
+  }
   return {
     props: {
       products: data.products,
     },
-    revalidate: 10 // durée avant le prochain generate de la page
+    revalidate: 10, // durée avant le prochain generate de la page
+    // notFound: true // return un true or false -> 404
+    // redirect: { destination: '/no-data' } // redirige la page et renvoit un redirect status code
   };
 }
 export default HomePage;
