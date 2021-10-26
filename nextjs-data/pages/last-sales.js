808fd7ed29c7cab2e4a52948038d92fde6a20e89
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-26 14:16:37 +0200

    section 5-110

--- nextjs-data/pages/last-sales.js
+++ nextjs-data/pages/last-sales.js
@@ -1,33 +1,52 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSalesPage() {
   const [sales, setSales] = useState(); // définit à undefined par défaut
-  const [isLoading, setIsLoading] = useState(false);
+  //   const [isLoading, setIsLoading] = useState(false);
+  const fetcher = (url) => fetch(url).then((res) => res.json())
+  const { data, error } = useSWR(
+    "https://nextjs-course-2fa09-default-rtdb.europe-west1.firebasedatabase.app/sales.json", fetcher);
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://nextjs-course-2fa09-default-rtdb.europe-west1.firebasedatabase.app/sales.json"
-    )
-      .then((response) => response.json()) // convertit la réponse en json
-      .then((data) => {
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+    // ici le useEffect est utiliser pour passer les données en tableau d'objets
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-  if (!sales) {
-    return <p>No daztz yet.</p>;
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch(
+  //       "https://nextjs-course-2fa09-default-rtdb.europe-west1.firebasedatabase.app/sales.json"
+  //     )
+  //       .then((response) => response.json()) // convertit la réponse en json
+  //       .then((data) => {
+  //         const transformedSales = [];
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  if (error) {
+    return <p>Failed to loasd.</p>;
+  }
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
   return (
     <ul>
